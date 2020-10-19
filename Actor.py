f3e92ca12d42a5936130365337f2614d525a1bc6
Jeremy Cowelchuk <cowelch1@msu.edu>
2020-10-19 10:44:21 -0400

    Added initial comments to the files

--- Actor.py
+++ Actor.py
@@ -1,1 +1,11 @@
+"""
+Actor.py
+
+Python application for recieving button input from another controller, reading them as vibrations and then sending an interpreted
+signal back to the source controller.
+
+Author: Jeremy Cowelchuk, (add your names here)
+"""
+
+
 
--- Observer.py
+++ Observer.py
@@ -1,1 +1,7 @@
+"""
+ Observer.py
+ 
+ Python application for running a game and transmitting button presses across a cloud service
 
+ Author: Jeremy Cowelchuk, (add your names here)
+ """

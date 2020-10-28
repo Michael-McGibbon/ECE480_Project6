c3ee0e32d3ba7a822e9214c654b526b3b175078e
Jeremy Cowelchuk <cowelch1@msu.edu>
2020-10-28 18:23:32 -0400

    Added test server from Twisted to confirm download

--- CloudService.py
+++ CloudService.py
@@ -5,3 +5,17 @@ Cloud service application which will act as a bridge between the Observer and Ac
 
 Author: Jeremy Cowelchuk, (add names here)
 """
+
+#imports
+from twisted.internet import protocol, reactor, endpoints
+
+class Echo(protocol.Protocol):
+    def dataReceived(self, data):
+        self.transport.write(data)
+
+class EchoFactory(protocol.Factory):
+    def buildProtocol(self, addr):
+        return Echo()
+
+endpoints.serverFromString(reactor, "tcp:1234").listen(EchoFactory())
+reactor.run()

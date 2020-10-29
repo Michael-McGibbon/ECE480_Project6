8a08b2d3a1aef94483fadc25205ef1a398a6bb2b
Jeremy Cowelchuk <cowelch1@msu.edu>
2020-10-28 20:34:46 -0400

    Actually Pushed Twisted integration into Observer.py

--- Observer.py
+++ Observer.py
@@ -6,46 +6,36 @@
  Author: Jeremy Cowelchuk, David Geisler, George Legacy (add your names here)
 """
 
-"""
-# adding this in, will be added to correct location when needed.
-
-# Imports
+# imports
 import pygame
 import time
 
+# twisted imports
+from twisted.internet import reactor
+from twisted.internet.protocol import Protocol
+from twisted.internet.endpoints import TCP4ClientEndpoint, connectProtocol
+from twisted.internet.task import LoopingCall
+
 # Constants
+DESIRED_FPS = 30.0 # 30 frames per second
 
-# initialize the game and necessary parts
-pygame.init()
+#initialize pygame
+pygame.init() # initialize the game and necessary parts
+screen = pygame.display.set_mode((500, 500)) # Set the width and height of the screen (width, height).
+pygame.display.set_caption("Observer") #name the program
 
-# Set the width and height of the screen (width, height).
-screen = pygame.display.set_mode((500, 500))
-
-pygame.display.set_caption("Observer")
-
-# Loop until the user clicks the close button.
-done = False
-
-# Initialize the joysticks.
-pygame.joystick.init()
-
+pygame.joystick.init() # Initialize the joysticks.
 joystick = pygame.joystick.Joystick(0)
 joystick.init()
 
-
-
-# -------- Main Program Loop -----------
-while not done:
-    #
-    # EVENT PROCESSING STEP
-    #
-    # Possible joystick actions: JOYAXISMOTION, JOYBALLMOTION, JOYBUTTONDOWN,
-    # JOYBUTTONUP, JOYHATMOTION
-    for event in pygame.event.get(): # User did something.
+def game_tick():
+    done = False # Loop until the user clicks the close button.
+    events = pygame.event.get()
+    for event in events:
+        # Process input events
         if event.type == pygame.QUIT: # If user clicked close.
             done = True # Flag that we are done so we exit this loop
         elif event.type == pygame.JOYBUTTONDOWN:
-
             # handle button presses, not dpad yet
             buttons = joystick.get_numbuttons()
             for i in range(buttons):
@@ -78,38 +68,35 @@ while not done:
                     #send the hat press to actor.py
                     #USE TWISTED HERE TO SEND TO ACTOR.PY
 
+        # elif event.type == pygame.JOYBUTTONUP:
+        #button go up :(
+    
+        
+    #redraw()
 
-                
+    if done == True:
+        #quit the game
+        pygame.quit()
+        reactor.stop()
 
-                       
-       # elif event.type == pygame.JOYBUTTONUP:
-            #button go up :(
 
 
-  
-# Close the window and quit.
-# If you forget this line, the program will 'hang'
-# on exit if running from IDLE.
-pygame.quit()
 
-#check
-#check2 David
-#check3 Michael
-#check4 Jeremy
-#check5 George
-"""
+# Set up a looping call every 1/30th of a second to run your game tick
+tick = LoopingCall(game_tick)
+tick.start(1.0 / DESIRED_FPS)
 
-from twisted.internet import reactor
-from twisted.internet.protocol import Protocol
-from twisted.internet.endpoints import TCP4ClientEndpoint, connectProtocol
-
+# Set up anything else twisted here, like listening sockets
 class Greeter(Protocol):
 
     def connectionMade(self):
         print("connected to server")
 
 
+#JEREMY: CHANGE "99.28.129.156" INTO "localhost"
+#EVERYONE ELSE: DO THE OPPOSITE OF ABOVE
 point = TCP4ClientEndpoint(reactor, "99.28.129.156", 25565)
 d = connectProtocol(point, Greeter())
 print("client")
 reactor.run()
+

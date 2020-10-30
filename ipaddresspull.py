9bf0a5e5d8e44995a8e3c26d5b8c49382253a4dd
legacyge <73082508+legacyge@users.noreply.github.com>
2020-10-30 13:25:50 -0400

    started practice with game and added icon for game windo

--- ipaddresspull.py
+++ ipaddresspull.py
@@ -1,3 +1,7 @@
+#This can be used to pull the IP address that can be passed to the Actor. CloudService and Observer can be ran from local host. Print the IP address at the top of the main screen of the game on Observer side 
+
+#Need requests library, can be downloaded with py -m pip install requests --user
+
 import requests
 from requests import get
 
